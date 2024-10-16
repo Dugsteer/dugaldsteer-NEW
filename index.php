a75070ac914b38378836b30e35aef4f565d8db90
Dugsteer <dugsteer@gmail.com>
2024-10-16 10:00:11 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dugald Steer</title>
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+
+
+<body>
+    <!-- partial:index.partial.html -->
+
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Dugald Steer</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Libre+Caslon+Text&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=EB+Garamond&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="stylesheets/styles.css">
+    </head>
+
+    <body>
+        <header>
+            <nav id="navbar">
+                <ul>
+
+                    <?php include "includes/bennu.php" ?>
+                    <li><a href="#biog">About</a></li>
+                    <li><a href="#books">Books</a></li>
+                    <li><a href="https://anglesmontalt.com/?language=eng.php" target=_blank>Teaching</a></li>
+                    <li><a href="#history">Translation</a></li>
+
+
+                </ul>
+            </nav>
+        </header>
+        <section id="welcome-section">
+            <h1>Dugald Steer</h1><br>
+            <p>His webpage under construction</p>
+        </section>
+        <section id="projects">
+            <h2 class="projects-section-header">Here's some stuff I've done/am doing<h2>
+                    <div class="projects-grid">
+                        <div class="project-tile"><a id="books"
+                                href="https://www.templarco.co.uk/templar-books?series=-33" target=_blank>
+                                <img src="https://live.staticflickr.com/65535/49247590053_bff1cebed1.jpg[/img][/url][url=https://flic.kr/p/2i2QLLe]OlogySeries[/url] by [url=https://www.flickr.com/photos/185411171@N07/]Dugald Steer[/url], on Flickr"
+                                    alt="A picture of the Ology book series featuring Dragonology, Pirateology and Wizardology"></a>
+                        </div>
+                        <div class="project-tile"><a id="teaching" href="https://www.anglesmontalt.com"
+                                target=_blank><img
+                                    src="https://live.staticflickr.com/65535/49248261717_283739ea5f.jpg[/img][/url][url=https://flic.kr/p/2i2UdqD]EnglishTeaching[/url] by [url=https://www.flickr.com/photos/185411171@N07/]Dugald Steer[/url], on Flickr"
+                                    alt="A screenshot of the front page of Dugald Steer's English Teaching website made in Wordpress"></a>
+                        </div>
+                        <div class="project-tile"><a link id="profile-link" href="https://codepen.io/Dugster"
+                                target=_blank><img
+                                    src="https://live.staticflickr.com/65535/49247590108_46dfc9c70c.jpg[/img][/url][url=https://flic.kr/p/2i2QLMb]Webdesign[/url] by [url=https://www.flickr.com/photos/185411171@N07/]Dugald Steer[/url], on Flickr"
+                                    alt="A screenshot of one of Dugald Steer's FreeCodeAcademy practise test websites about Barcelona"></a>
+                        </div>
+                        <div class="project-tile"><a href=""><a id="history"><img
+                                        src="https://live.staticflickr.com/65535/49248055331_a4f9d1f8ab.jpg[/img][/url][url=https://flic.kr/p/2i2Ta5g]History[/url] by [url=https://www.flickr.com/photos/185411171@N07/]Dugald Steer[/url], on Flickr"
+                                        alt="" width=500px></a>
+                        </div>
+                    </div>
+        </section>
+    </body>
+
+    </html>
+    <!-- partial -->
+
+</body>
+
+</html>
